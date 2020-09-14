cdbeb819ec396b762f3b7539f9611f6325d7ae77
RichardT15 <richard.tham@polymtl.ca>
2020-09-14 18:29:27 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,7 +17,7 @@ def to_degrees(angle_rads: float) -> tuple:
     deg=0
     minutes=0
     seconds=0
-    return (angle_rads*(180/math.pi)
+    return (angle_rads*(180/math.pi))
 
 
 def to_celsius(temperature: float) -> float:
